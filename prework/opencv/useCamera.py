3390ac52290db20d829c027d4bf8e3dd1462fd83
abhi <rudraxx@gmail.com>
2017-01-13 18:59:55 -0800

    Making showing only the grayscale video in useCamera.py

--- prework/opencv/useCamera.py
+++ prework/opencv/useCamera.py
@@ -1,17 +1,18 @@
+# Testing video capture with opencv2.
+
 
 import cv2
-import matplotlib.pyplot as py
 
-# Start the camera
-cv2.VideoCapture(0)[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dcap = [2~cv2.c[2Videocapture(0)
-[D[D[D[D[D[D[D[D[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[1;2D[1;2CC[Ca[C[C[C[C[C[C[C[C[C[C[D[D
+cap = cv2.VideoCapture(0)
 
-while True:
-[D[A[C[C[C[C[C[C([C[C[D[DTrue):
-   ret,frame = cap.read()
-   cv2.imshow('myfig',frame)
-   if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
-
+while True :
+    ret,frame = cap.read()
+    grayframe = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    
+    # Show this image
+    cv2.imshow('fig',frame)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+    
 cap.release()
 cv2.destroyAllWindows()
