661e7f164cadca0c1f2ce615c1da6f651c6c6595
abhi <rudraxx@gmail.com>
2017-01-13 21:15:08 -0800

    Forgot to add the activation to the convolutional2d layer. that was causing the values to be ~65% accuracy, instead of ~99%

--- prework/keras/first_test_cnn.py
+++ prework/keras/first_test_cnn.py
@@ -9,8 +9,8 @@ from keras.datasets import mnist
 
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dropout
 
@@ -18,7 +18,8 @@ from keras.utils import np_utils
 import numpy
 import matplotlib.pyplot as plt
 
-
+seed = 7
+numpy.random.seed(seed)
 
 (x_train,y_train),(x_test,y_test) = mnist.load_data()
 #asd = plt.imshow(x_train[1],cmap='gray')
@@ -29,11 +30,11 @@ numTrainImages = x_train.shape[0]
 numTestImages = x_test.shape[0]
 
 # Reshape the data to be a column vector
-x_train = x_train.reshape(numTrainImages,28,28,1)
-x_test  = x_test.reshape(numTestImages,28,28,1)
+x_train = x_train.reshape(numTrainImages,28,28,1).astype('float32')
+x_test  = x_test.reshape(numTestImages,28,28,1).astype('float32')
 
-#x_train = x_train/255
-#x_test = x_test/255
+x_train = x_train/255
+x_test = x_test/255
 
 
 
@@ -41,11 +42,12 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 numClasses = y_train.shape[1]
 
-#Create a basic model
-def baseline_model():
+#Create a cnn model
+def cnn_model():
     model = Sequential()
     model.add(Convolution2D(32,5,5,
                             border_mode='valid',
+                            activation = 'relu',
                             input_shape= (28,28,1)))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Dropout(0.2))
@@ -53,7 +55,6 @@ def baseline_model():
     model.add(Dense(128,activation='relu'))
     
     model.add(Dense(numClasses,
-                    init='normal',
                     activation='softmax'))
     # Compile the model
     model.compile(optimizer='adam',
@@ -62,7 +63,7 @@ def baseline_model():
     return model
     
 #Build the model
-model = baseline_model()
+model =     cnn_model()
 
 #Fit the model
 history = model.fit(x_train,y_train,batch_size=200,nb_epoch=10,
@@ -70,7 +71,7 @@ history = model.fit(x_train,y_train,batch_size=200,nb_epoch=10,
 
 #Final evaluation of the model
 scores  = model.evaluate(x_test,y_test,verbose=0)
-
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
 
 
     
